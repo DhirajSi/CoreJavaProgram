233508c4a4f5954e7d6649eda61e8c8ed522cb89
Dhiraj <singhdhiraj689@gmail.com>
2021-06-10 23:33:33 +0530

    comment add in bubble sorting program

--- src/corejava/basicprogramcode/BubbleSorting.java
+++ src/corejava/basicprogramcode/BubbleSorting.java
@@ -3,6 +3,8 @@ package corejava.basicprogramcode;
 public class BubbleSorting {
 
 	public static void main(String[] args) {
+		
+		//This is Bubble sorting logic
 		int no[] = { 12, 4, 56, 3, 11, 19 };
 
 		for (int i = 0; i < no.length - 1; i++) {
